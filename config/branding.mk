71c347b35c2e27103854f6c320498187f85d4409
timocapa <timo.capa@gmail.com>
2020-01-21 14:08:23 +0100

    AVOCADO, yo

--- config/branding.mk
+++ config/branding.mk
@@ -2,7 +2,7 @@
 DU_BASE_VERSION = v14.0
 
 ifndef DU_BUILD_TYPE
-    DU_BUILD_TYPE := UNOFFICIAL
+    DU_BUILD_TYPE := AVOCADO
 endif
 
 # Only include DU-Updater for official, weeklies, and rc builds
